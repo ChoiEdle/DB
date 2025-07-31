45b1dbcd19d8186788ace5e8084e11651b78099b
choi <cjy1749@naver.com>
2025-07-31 18:18:30 +0900

    07-31 commit

--- mysql_80_실습.sql
+++ mysql_80_실습.sql
@@ -1979,10 +1979,76 @@ select * from employee;
 
 show tables;
 
+use hrdb2019;
+select database();
+show tables;
+desc member;
+select * from member;
+
+select member_id, name, email, created_at
+from member
+where name = '홍길동';
+
+select member_id, name, email, created_at
+from member
+where member_id = 1;
+
+insert into member(name, email, created_at)
+values("고길동", "go@gil.dong", now());
+
+update member
+set name = '홍길순', email = 't@es.t'
+where member_id = 1;
+
+update score_member
+set kor = 50, eng = 50, math = 50
+where mid = 'M0001';
+
+create table score_member(
+	mid			char(5)		primary key,	-- 'M0001' 형식의 트리거 생성
+    name		varchar(10)	not null,
+    department	varchar(20),
+    kor			decimal(6,2)	default 0.0,
+    eng			decimal(6,2)	default 0.0,
+    math		decimal(6,2)	default 0.0,
+    mdate		datetime
+);
+
+desc score_member;
+select count(*) from score_member;
+select mid, name, department, kor, eng, math, mdate
+from score_member;
+select row_number() over(),
+				mid, name, department, kor, eng, math, mdate
+				from score_member;
+
+
+select * from score_member;
+select * from information_schema.triggers;
+delete from score_member where mid = 'M0003';
 
 
 
 
+-- trigger 생성 : 여러개의 sql문 포함
+delimiter $$
+
+create trigger trg_score_member_mid
+before insert on score_member	-- 테이블명
+for each row
+begin
+declare max_code int;	-- 'M0001'
+
+-- 현재 저장된 값 중 가장 큰 값을 가져옴
+select ifnull(max(convert(right(mid,4), unsigned)),0)
+into max_code
+from score_member;
+
+-- 'M0001' 형식으로 아이디 생성, lpad(값, 크기, 채워지는 문자형식) : 1
+set new.mid = concat('M', lpad(max_code + 1, 4, '0'));
+
+end $$
+delimiter ;
 
 
 
